e500486633e188ab4706834e455a6ace4215e200
Tom Milner <tmilner@mdsol.com>
2023-11-14 14:20:51 +0000

    Icon updates

--- lib/weather-line-updater.cc
+++ lib/weather-line-updater.cc
@@ -81,6 +81,41 @@ void WeatherLineUpdater::update()
             this->current_display++;
     }
 
+    if (((now - this->last_weather_update) / 1s) > this->update_weather_after_seconds)
+    {
+        std::cout << "Fetching wether data from " << this->url << std::endl;
+        try
+        {
+            Json::Value jsonData = fetcher->fetch(this->url);
+
+            const std::string condition(jsonData["weather"][0]["description"].asString());
+            const std::string weather_icon(jsonData["weather"][0]["icon"].asString());
+            const double kevinScale = 273.15;
+            const double temp = jsonData["main"]["temp"].asDouble() - kevinScale;
+
+            std::stringstream temp_str_stream;
+            temp_str_stream << std::fixed << std::setprecision(0) << temp;
+            std::string temp_str = temp_str_stream.str();
+
+            std::cout << "\tCondition: " << condition << std::endl;
+            std::cout << "\tTemp: " << temp << std::endl;
+            std::cout << "\tIcon: " << weather_icon << std::endl;
+
+            std::cout << std::endl;
+
+            this->weather_image.clear();
+            this->weather_image.append(weather_icon);
+            this->weather_line.clear();
+            this->weather_line.append(temp_str).append("℃");
+            this->last_weather_update = now;
+        }
+        catch (std::runtime_error &e)
+        {
+            printf("Failed to fetch Weather\n");
+        }
+    }
+
+    // switch on enum pls
     if (this->current_display == 1)
     {
         this->current_image.clear();
@@ -88,49 +123,15 @@ void WeatherLineUpdater::update()
         this->current_line.clear();
         this->current_line.append(date::format("%R", date::floor<std::chrono::milliseconds>(now)));
     }
-    if (this->current_display == 2)
+    else if (this->current_display == 2)
     {
         this->current_image.clear();
         this->current_image.append(this->date_image);
         this->current_line.clear();
-        this->current_line.append(date::format("%D", date::floor<std::chrono::milliseconds>(now)));
+        this->current_line.append(date::format("%d %b", date::floor<std::chrono::milliseconds>(now)));
     }
     else if (this->current_display == 0)
     {
-        if (((now - this->last_weather_update) / 1s) > this->update_weather_after_seconds)
-        {
-            std::cout << "Fetching wether data from " << this->url << std::endl;
-            try
-            {
-                Json::Value jsonData = fetcher->fetch(this->url);
-
-                const std::string condition(jsonData["weather"][0]["description"].asString());
-                const std::string weather_icon(jsonData["weather"][0]["icon"].asString());
-                const double kevinScale = 273.15;
-                const double temp = jsonData["main"]["temp"].asDouble() - kevinScale;
-
-                std::stringstream temp_str_stream;
-                temp_str_stream << std::fixed << std::setprecision(0) << temp;
-                std::string temp_str = temp_str_stream.str();
-
-                std::cout << "\tCondition: " << condition << std::endl;
-                std::cout << "\tTemp: " << temp << std::endl;
-                std::cout << "\tIcon: " << weather_icon << std::endl;
-
-                std::cout << std::endl;
-
-                this->weather_image.clear();
-                this->weather_image.append(weather_icon);
-                this->weather_line.clear();
-                this->weather_line.append(temp_str).append("℃");
-                this->last_weather_update = now;
-            }
-            catch (std::runtime_error &e)
-            {
-                printf("Failed to fetch Weather\n");
-            }
-        }
-
         this->current_image.clear();
         this->current_image.append(this->weather_image);
         this->current_line.clear();
