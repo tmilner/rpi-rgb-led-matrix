c04af47675c8eb6d6f05275ccbedd3d55dae7961
Tom Milner <tmilner@mdsol.com>
2023-11-22 12:43:15 +0000

    Image cleanup

--- lib/music-line.cc
+++ lib/music-line.cc
@@ -40,7 +40,7 @@ void MusicLine::render(FrameCanvas *offscreen_canvas)
     }
     this->renderLine(offscreen_canvas);
     offscreen_canvas->SetPixels(0, this->y, 13, 16, 0, 0, 0);
-    CopyImageToCanvas(this->getIcon(), offscreen_canvas, 1, this->y + 2);
+    CopyImageToCanvas(this->getIcon(), offscreen_canvas, 0, this->y + 1);
     rgb_matrix::DrawLine(offscreen_canvas, 13, this->y, 13, this->y + 16, Color(130, 100, 73));
 }
 
